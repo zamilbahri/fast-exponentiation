0a971291ca5da0f2d9c643510ebe24cd85f0bec1
Zamil Bahri <zamilbahri@gmail.com>
2026-02-15 23:05:03 -0500

    Configure project for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/fast-exponentiation/',
   plugins: [react(), tailwindcss()],
 });
